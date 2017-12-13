1782f9c8dae99641548983e7d8cfcb154eaa55b8
Danilo Sanchez <df.sanchez@outlook.com>
2017-12-13 16:00:47 +0100

    Added list items and remove functionality.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+const store = configureStore();
+
+store.subscribe(() => {
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 400, createdAt: 1000 }));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('bill'));
+}, 3000);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter/>
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
